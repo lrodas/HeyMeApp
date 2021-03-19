421b2db2d6ae5fa6d62eb7ef794815cbc59e7f7d
Luis Emmanuel Rodas Camposeco <lrodasc1@miumg.edu.gt>
2021-03-18 21:40:46 -0600

    actualizacion de full calendar y clave para recaptcha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: 'https://brainology.com.gt/heyme/api/v1',
   paypalClientId: 'AZvFnePj9S3gN5oqGNesv0ACk95zrk-mzisvnKYeyCBb5dFw6_UsLaGkkkh07h7ZlVPbNpKzBUG352du',
-  dataSiteKey: '6LcPmesUAAAAABy9HCfDX5QkIQI7lokXODUx4CnA'
+  dataSiteKey: '6LenbIUaAAAAAJvrnkstAicYTKBduh-C9hwTrOvx'
 };
